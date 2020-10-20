ba9679e3dc2b8638a9209223d51f7e0e357e9cdb
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2020-10-20 19:08:55 -0300

    vetores
    inclusao dos exercicios de vetores

--- Lista 01/ex18.c
+++ Lista 01/ex18.c
@@ -1,33 +1,77 @@
 /*
-24.	Fazer uma programa que codifica um string pelo método  ZENIT-POLAR 
+18.	Ler 10 números a serem digitados pelo usuário e armazená-los em um vetor. 
+a) Exibir a quantidade de vezes que o número 3 está presente no vetor.
+b) Pergunte ao usuário qual número ele quer pesquisar (no lugar do número 3), e diga quantas vezes este número está presente no vetor.
 
-    Z   E   N   I   T 
-    P   O   L   A   R 
-	
-Ou seja, onde aparece um 'Z' no string de entrada, aparecerá um 'P' na saída; e, semelhantemente, onde aparece um 'P' no string de entrada, aparecerá um 'Z' na saída. Isto deve ocorrer para todos os pares em ZENIT-POLAR, maiúsculas e minúsculas. As outras letras não são alteradas. 
-
-Por exemplo, o string "Parabens a voce" é codificado para "Zitibols i veco". Ao se aplicar duas vezes a transformação, obtém-se o mesmo string, ou seja, a codificação de "Zitibols i veco" é "Parabens a voce".
 
 solucao em:
-
-
+https://repl.it/@ViniciusB1/LDA-AT1-ex18#main.c
 */
 
 /*
 planejamento:
-
+declarar vetor inteiro com 10 elementos;
+declara variavel inteira para pesquisa, contador e para controle de estrutura de repeticao;
+criar estrutura de repeticao com condicao de parada apos 10 execucoes
+	solicitar numero para usuario
+	ler numero do usuario;
+	verificar se é igual a 3;
+		se sim, somar 1 ao contador;
+	incrementar variavel de controle
+exibir o numero de vezes que o numero 3 foi encontrado a partir do contador;
+solicitar numero para pesquisa do usuario
+criar estrutura de repeticao com condicao de parada apos 10 execucoes
+	zerar contador;
+	verificar se cada um dos numeros do vetor é igual ao numero informado pelo usuario;
+		se sim, somar 1 ao contador;
+	incrementar variavel de controle
+exibir o numero de vezes que o numero informado foi encontrado a partir do contador
 
 
 */
 
-
 #include <stdio.h>
-#include <string.h>
 
 int main(void) 
-
-
 {
+	int vetor[10];
+	int i=0, pesquisa=3, cont=0;
+	
+	//QUESTÃO A
+	do
+	{
+		printf("Digite o #%d numero: ", (i+1));
+		scanf("%d", &vetor[i]);
+		fflush(stdin);
+		if (vetor[i]==pesquisa)
+		{
+			cont+=1;
+		}
+		i+=1;
+	} while (i<10);
 
-    return 0;
+	printf ("\nNumero de vezes que %d foi encontrado = %d\n", pesquisa, cont);
+
+	/*
+	for (i=0; i<10; i++)
+	{
+		printf ("%d ", vetor[i]);
+	}
+	*/
+
+
+	//QUESTAO B
+	printf ("\nDigite o numero que deseja pesquisar: ");
+	scanf ("%d", &pesquisa);
+
+	for (i=0, cont=0; i<10; i++)
+	{
+		if (vetor[i]==pesquisa)
+		{
+			cont+=1;
+		}
+	}
+
+	printf ("Numero de vezes que %d foi encontrado = %d", pesquisa, cont);
+  	return 0;
 }
--- Lista 01/ex19.c
+++ Lista 01/ex19.c
@@ -1,33 +1,73 @@
 /*
-24.	Fazer uma programa que codifica um string pelo método  ZENIT-POLAR 
-
-    Z   E   N   I   T 
-    P   O   L   A   R 
-	
-Ou seja, onde aparece um 'Z' no string de entrada, aparecerá um 'P' na saída; e, semelhantemente, onde aparece um 'P' no string de entrada, aparecerá um 'Z' na saída. Isto deve ocorrer para todos os pares em ZENIT-POLAR, maiúsculas e minúsculas. As outras letras não são alteradas. 
-
-Por exemplo, o string "Parabens a voce" é codificado para "Zitibols i veco". Ao se aplicar duas vezes a transformação, obtém-se o mesmo string, ou seja, a codificação de "Zitibols i veco" é "Parabens a voce".
+19.	Ler 10 números a serem digitados pelo usuário e armazená-los em um vetor.
+a) Diga qual é o maior dos 10 números.
+b) Diga qual é o menor dos 10 números.
 
 solucao em:
-
+https://repl.it/@ViniciusB1/LDA-AT1-ex19#main.c
 
 */
 
 /*
 planejamento:
+declarar vetor inteiro com 10 posicoes;
+declarar variavel inteira para maior, menor e controle de estrutura de repeticao;
 
+solicitar 1 numero do usuario;
+ler 1 numero do usuario;
+maior e menor recebem o primeiro numero;
+
+estrutura de repeticao com inicio na 2 posicao e parada apos 10 posicao;
+	solicitar numero para usuario
+	ler numero;
+		verificar se o numero é maior que aquele que consta na variavel maior;
+			se sim, substituir o numero existente;
+		verificar se o numero é menor que aquele que consta na variavel menor;
+			se sim, substituir o numero existente;
+
+exibir o maior numero e o menor numero a partir das variaveis maior e menor declaradas;
 
 
 */
 
 
 #include <stdio.h>
-#include <string.h>
 
 int main(void) 
-
-
 {
+	int numeros[10];
+	int i=0, maior, menor;
+
+	printf("Digite o #%d numero: ", (i+1));
+	scanf("%d", &numeros[i]);
+	fflush(stdin);
+	maior=numeros[i];
+	menor=numeros[i];
+	
+	for (i=1; i<10; i++)
+	{
+		printf("Digite o #%d numero: ", (i+1));
+		scanf("%d", &numeros[i]);
+		fflush(stdin);
+		if (numeros[i]>maior)
+		{
+			maior=numeros[i];
+		}
+		else if (numeros[i]<menor)
+		{
+			menor=numeros[i];
+		}
+	}
+	
+	/* apenas para conferir vetor
+	printf(vetor numeros: );
+	for (i=0; i<10; i++)
+	{
+		printf("%d ", numeros[i]);
+	}
+	*/
+
+	printf ("\nMaior numero digitado: %d\nMenor numero digitado: %d", maior, menor);
 
     return 0;
 }

d477a06233159c03554ff28fdb3ae447f3a5fb0c
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2020-10-20 17:59:25 -0300

    Update ex23.c
    ex23

--- Lista 01/ex23.c
+++ Lista 01/ex23.c
@@ -7,7 +7,7 @@ da
 João
 
 solucao em:
-
+https://repl.it/@ViniciusB1/LDA-L1-ex23#main.c
 
 */
 
@@ -23,9 +23,58 @@ planejamento:
 #include <string.h>
 
 int main(void) 
-
-
 {
+	int i=0, j=0, aux=0;
+	char nome[100] = "Maria da Silva Melo";
+	int posica[100];
+	printf("Digite o nome completo: ");
+	gets(nome);
 
-    return 0;
+	/*for (i=strlen(nome), aux=strlen(nome); i>=0; i--)
+	{
+		if (nome[i]!=' ')
+		{
+			//printf("\ni = %d ",i);
+			//printf("caractere asc2 %d ", nome[i]);
+			//printf("aux %d\n",aux);
+		}
+		else
+		{
+			//puts("entrou no else");
+			j=i+1;
+			while (j!=aux)
+			{
+				printf("%c", nome[j]);
+				j+=1;
+			}
+			puts("");
+			aux=i;
+		}
+		
+	}*/
+
+	for (i=strlen(nome), aux=strlen(nome); i>=0; i--)
+	{
+		if (nome[i]==' ')
+		{
+			//puts("entrou no else");
+			j=i+1;
+			while (j!=aux)
+			{
+				printf("%c", nome[j]);
+				j+=1;
+			}
+			puts("");
+			aux=i;
+		}
+		
+	}
+	//imprime primeiro nome
+	while (i!=aux)
+	{
+		printf("%c", nome[i]);
+		i+=1;
+	}
+
+  	return 0;
 }
